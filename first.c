5ab701794ecb3545a628bc533988076fc4f45ee6
Sharia Hussain <sh1366@butter.cs.rutgers.edu>
2020-12-05 18:12:36 -0500

    Read from File Pointer Setup

--- first.c
+++ first.c
@@ -1,7 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct Node
+{
+    int data;
+    char varName[50];
+    struct Node* next;
+
+} Node;
+
+Node* insert(Node* head, char* name, int data)
+{
+    Node* insertedNode = (Node*) malloc(sizeof(Node));
+    insertedNode ->data = data;
+    insertedNode->next = NULL;
+    strcpy(insertedNode->varName, name);
+    
+    if(head==NULL)
+    {
+        return insertedNode;
+    }
+
+    Node* ptr = head;
+    while(ptr!=NULL)
+    {
+        ptr = ptr->next;
+        
+        if(ptr == NULL)
+        {
+            ptr->next = insertedNode;
+            return head;
+        }
+    }
+
+    //else attach
+}
+
+int exponent(int power)
+{
+    return 1 << power;
+}
+
+int** arr = NULL;
 
 int main(int argc, char* argv[])
 {
+    FILE* fp = fopen(argv[1], "r");
     
+    char line[100] = "";
+    char argument[25] = "";
+    int rows = 0;
+    int cols = 0;
+    int** truthTable = NULL;
+    //fgets(line, 100,fp);
+    
+
+    while(fgets(line, 100,fp)!=NULL)
+    {
+        sscanf(line, "%s", argument);
+
+        //remove this from loop
+        if(strcmp(argument, "INPUTVAR")==0)
+        {
+            //make sure im not mallocing multiple times;
+            int inputVariables = 0;
+            sscanf(line, "%s %i", argument, &inputVariables);
+            rows = exponent(inputVariables);
+            cols = inputVariables+1;
+
+            truthTable = malloc(rows*sizeof(int*));
+            for(int i = 0; i < rows; i++)
+            {
+                truthTable[i] = malloc(cols*sizeof(int));
+            }
+
+            //change output size???
+            //cols = inputVariables+1;
+
+        }
+        else if(strcmp(argument, "OUTPUTVAR")==0)
+        {
+            
+        }
+        else if(strcmp())
+        {
+
+        }
+
+        printf("%s \n", argument);
+    }
 }
